f62a7806a62c3ab137badd1355bbd2cf7fb49c55
once-ler <htao@eztier.com>
2020-02-11 19:37:50 -0500

    Start refactor to compile to linux

--- tinycrypto/include/program.h
+++ tinycrypto/include/program.h
@@ -1,4 +1,10 @@
+#define DLLEXPORT __attribute__((visibility("default")))
+
+#ifdef _WIN32
 #include <windows.h>
+#define DLLEXPORT __declspec( dllexport )
+#endif
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,15 +18,15 @@ extern unsigned char* decrypted_data;
 int aes_init(unsigned char *key_data, int key_data_len, unsigned char *salt, char *hiv, EVP_CIPHER_CTX *e_ctx, EVP_CIPHER_CTX *d_ctx);
 unsigned char *aes_encrypt(EVP_CIPHER_CTX *e, unsigned char *plaintext, int *len);
 int set_hex(char *in, unsigned char *out, int size);
-__declspec( dllexport ) char* get_shared_secret(const char* keyfile, const char* ssecret);
+DLLEXPORT char* get_shared_secret(const char* keyfile, const char* ssecret);
 void extract_shared_secret(char* secret, unsigned char* key_data, char* iv);
 
 // small file encrypt
-__declspec( dllexport ) int EncryptFileX(unsigned char* data, unsigned long datasize, char* private_key, char* shared_secret, char* filename);
+DLLEXPORT int EncryptFileX(unsigned char* data, unsigned long datasize, char* private_key, char* shared_secret, char* filename);
 // large file encrypt
-__declspec( dllexport ) int EncryptFileInit(char* private_key, char* shared_secret, char* filename);
-__declspec( dllexport ) int EncryptFileUpdate(unsigned char* data, unsigned long datasize);
-__declspec( dllexport ) int EncryptFileFinal();
+DLLEXPORT int EncryptFileInit(char* private_key, char* shared_secret, char* filename);
+DLLEXPORT int EncryptFileUpdate(unsigned char* data, unsigned long datasize);
+DLLEXPORT int EncryptFileFinal();
 
-__declspec( dllexport ) unsigned char* DecryptFileX(char* private_key, char* shared_secret, char* filename, int* decrypted_size);
-__declspec( dllexport ) void FreeDecryptedMemory();
+DLLEXPORT unsigned char* DecryptFileX(char* private_key, char* shared_secret, char* filename, int* decrypted_size);
+DLLEXPORT void FreeDecryptedMemory();
